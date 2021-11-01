c4a8a485516640cdb55aa298361b7290e091e48e
nparashar150 <nparashar150@gmail.com>
2021-11-01 20:01:17 +0530

    Map Skills Data

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import {
   SkillsSectionWrapper,
   SkillsWrapper,
@@ -5,51 +6,31 @@ import {
   SkillsFilter,
 } from "./SkillsElements";
 
+import SkillsData from "./SkillsData.json";
+
 const Skills = () => {
+  let [skillsInfo, setSkillsInfo] = useState([]);
+  const getSkillsInfo = async () => {
+    setSkillsInfo(await SkillsData.data);
+  };
+
+  useEffect(() => {
+    getSkillsInfo();
+  }, []);
+
   return (
     <>
       <SkillsSectionWrapper>
         <SkillsWrapper>
-          <SkillName>
-            <SkillsFilter width={"75%"} widthLeft={"25%"}>
-              HTML
-            </SkillsFilter>
-          </SkillName>
-          <SkillName>
-            <SkillsFilter width={"80%"} widthLeft={"20%"}>
-              CSS
-            </SkillsFilter>
-          </SkillName>
-          <SkillName>
-            <SkillsFilter width={"70%"} widthLeft={"30%"}>
-              Javascript
-            </SkillsFilter>
-          </SkillName>
-          <SkillName>
-            <SkillsFilter width={"60%"} widthLeft={"40%"}>
-              ReactJS
-            </SkillsFilter>
-          </SkillName>
-          <SkillName>
-            <SkillsFilter width={"65%"} widthLeft={"35%"}>
-              Styled Components
-            </SkillsFilter>
-          </SkillName>
-          <SkillName>
-            <SkillsFilter width={"65%"} widthLeft={"35%"}>
-              ExpressJS
-            </SkillsFilter>
-          </SkillName>
-          <SkillName>
-            <SkillsFilter width={"60%"} widthLeft={"40%"}>
-              MongoDb
-            </SkillsFilter>
-          </SkillName>
-          <SkillName>
-            <SkillsFilter width={"80%"} widthLeft={"20%"}>
-              Bash Scripting
-            </SkillsFilter>
-          </SkillName>
+          {Object.values(skillsInfo).map((element) => {
+            return (
+              <SkillName>
+                <SkillsFilter width={element.percentage} widthLeft={element.left}>
+                  {element.name}
+                </SkillsFilter>
+              </SkillName>
+            );
+          })}
         </SkillsWrapper>
       </SkillsSectionWrapper>
     </>
