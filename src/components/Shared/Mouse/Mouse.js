91d7908ee1728873dc2d284600aace1f8b5e1d21
nparashar150 <nparashar150@gmail.com>
2022-01-14 16:56:26 +0530

    Hide Custom Mouse Cursor on Mobile

--- src/components/Shared/Mouse/Mouse.js
+++ src/components/Shared/Mouse/Mouse.js
@@ -1,23 +1,31 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./Mouse.scss";
 
 const Mouse = () => {
+  const [isMobile, setIsMobile] = useState(false);
   const cursorRef = useRef(null);
 
   useEffect(() => {
-    document.addEventListener("mousemove", (event) => {
-      const { clientX, clientY } = event;
-      const mouseX = clientX - cursorRef.current.clientWidth / 2;
-      const mouseY = clientY - cursorRef.current.clientHeight / 2;
-      cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-    });
+    isMobile &&
+      document.addEventListener("mousemove", (event) => {
+        const { clientX, clientY } = event;
+        const mouseX = clientX - cursorRef.current.clientWidth / 2;
+        const mouseY = clientY - cursorRef.current.clientHeight / 2;
+        cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+      });
     return () => {};
+  }, [isMobile]);
+
+  useEffect(() => {
+    window.screen.width >= 768 ? setIsMobile(true) : setIsMobile(false);
   }, []);
 
   return (
-    <>
-      <div className="cursor" ref={cursorRef} />
-    </>
+    isMobile && (
+      <>
+        <div className="cursor" ref={cursorRef} />
+      </>
+    )
   );
 };
 
